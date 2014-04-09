57bb9029a05de0a0fe6274921cf28132254d28fd
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-09 01:27:03 -0400

    Made nav bar sticky. I hope this was the right way to do that.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     
-    console.log('Message for you sir');
-    
-    
-    
+    var stickyTop = $('.names').offset().top;
+    $(window).scroll(function(){
+        var windowTop = $(window).scrollTop();
+        if (stickyTop < windowTop) {
+            $('.names').css({ position: 'fixed', top: 0});
+        }
+        else {
+            $('.names').css('position', 'static');
+        }
+    });
     
     
     
