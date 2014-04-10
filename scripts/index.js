501c16cbd0b314bb2e7f65f9f8416c9bd6952b6a
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-10 01:26:21 -0400

    tried to add parallax scrolling

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,57 @@ $(document).ready(function(){
         }
     });
     
+// INIT SKROLLR
+var s = skrollr.init();    
+
+// REFRESH SKROLLR AFTER RESIZING OUR SECTIONS
+
+s.refresh($('.homeSlide'));  
     
     
     
+( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		// Init Skrollr
+		
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    
+	    
+	}
+		
+} )( jQuery );    
+    
+
 });
