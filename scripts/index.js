1b34c3637b5c60edb5c26acd9748b07b90761c1a
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-14 00:32:12 -0400

    Parallax is not working still

--- scripts/index.js
+++ scripts/index.js
@@ -11,12 +11,48 @@ $(document).ready(function(){
         }
     });
     
-// INIT SKROLLR
-var s = skrollr.init();    
-
-// REFRESH SKROLLR AFTER RESIZING OUR SECTIONS
-
-s.refresh($('.homeSlide'));  
+( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		// Init Skrollr
+		var s = skrollr.init();
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    s.refreas($('.homeSlide'));
+	    
+	}
+		
+} )( jQuery );
     
     
 
