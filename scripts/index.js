4e7d4c4f396bd1e205c1c6fee9f921c9b822dc22
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-17 04:19:49 -0400

    css and parallax

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,49 @@
 $(document).ready(function(){
     
-    var stickyTop = $('.names').offset().top;
-    $(window).scroll(function(){
-        var windowTop = $(window).scrollTop();
-        if (stickyTop < windowTop) {
-            $('.names').css({ position: 'fixed', top: 0});
-        }
-        else {
-            $('.names').css('position', 'static');
-        }
-    });
+
+( function( $ ) {
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		// Init Skrollr
+		var s - skrollr.int({
+            forceHeight: false
+        })
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    s.refresh($('.homeSlide'));
+	    
+	}
+		
+} )( jQuery );
+    
     
