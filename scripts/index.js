3082a765664d80085ff49a58015ba1fc6c9d9a1c
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-23 15:27:47 -0400

    Trying to figure out scrolltop

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,5 @@
 $(document).ready(function (){
-    $("#click").click(function (){
-        $('html, body').animate({
-                scrollTop: $("#div2").offset().top
-                 }, 2000);
-        return false;
+    $("button").click(function(){
+        alert($("#slide1").scrollTop());
     });
 });
