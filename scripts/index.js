09bc81a955249c3578023468396297cd1536e6db
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-24 11:19:44 -0400

    Worked on parallax again

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-	
+
 	$('nav ul li a').click(function(){
-	
+
 		var el = $(this).attr('href');
 		var elWrapped = $(el);
-		
+
 		scrollToDiv(elWrapped,40);
-		
+
 		return false;
-	
+
 	});
-	
+
     $(function(){
         var s = skrollr.init({
             render: function(data) {
@@ -21,4 +21,3 @@ $(document).ready(function(){
     });
  
 });
-
