20d339e3bc8d0a3e5f512a190205bd151eba2e64
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-24 00:59:51 -0400

    figured out scrollTop

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,30 @@
-$(document).ready(function (){
-    $("button").click(function(){
-        alert($("#slide1").scrollTop());
-    });
+$(document).ready(function(){
+	
+	$('nav ul li a').click(function(){
+	
+		var el = $(this).attr('href');
+		var elWrapped = $(el);
+		
+		scrollToDiv(elWrapped,40);
+		
+		return false;
+	
+	});
+	
+	function scrollToDiv(element,navheight){
+	
+		
+	
+		var offset = element.offset();
+		var offsetTop = offset.top;
+		var totalScroll = offsetTop-navheight;
+		
+		$('body,html').animate({
+				scrollTop: totalScroll
+		}, 500);
+	
+	}
+	
+	
+	
 });
