18fe35d2da38a16d571120c038c74123a4f7cf05
sarahbrand4 <sarah2@Sarahs-MacBook-Pro.local>
2014-04-24 10:58:46 -0400

    Made parallax scrolling better

--- scripts/index.js
+++ scripts/index.js
@@ -11,20 +11,14 @@ $(document).ready(function(){
 	
 	});
 	
-	function scrollToDiv(element,navheight){
-	
-		
-	
-		var offset = element.offset();
-		var offsetTop = offset.top;
-		var totalScroll = offsetTop-navheight;
-		
-		$('body,html').animate({
-				scrollTop: totalScroll
-		}, 500);
-	
-	}
-	
-	
-	
+    $(function(){
+        var s = skrollr.init({
+            render: function(data) {
+            }
+        });
+        
+        
+    });
+ 
 });
+
